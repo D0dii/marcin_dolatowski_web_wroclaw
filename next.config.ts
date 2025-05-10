662c961f48287ca86b1437211d1b7fc71ed4a2c8
D0dii <marcindolatowski123@gmail.com>
2025-05-10 17:05:27 +0200

    fix: change output to standalone

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig = {
   images: { unoptimized: true },
   assetPrefix: isProd ? "/marcin_dolatowski_web_wroclaw/" : "",
   basePath: isProd ? "/marcin_dolatowski_web_wroclaw" : "",
+  output: "standalone",
 };
 
 export default nextConfig;
