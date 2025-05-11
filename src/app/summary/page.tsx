11da8c17e8ec31f97be0b4475297d169eaef7113
D0dii <marcindolatowski123@gmail.com>
2025-05-11 18:49:11 +0200

    fix: delete delay for clearing cart

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -28,9 +28,8 @@ export default function SummaryPage() {
       totalPrice: total,
       items: items,
     } satisfies Order;
-    setTimeout(() => {
-      clearCart();
-    }, 2000);
+
+    clearCart();
     addOrder(newOrder);
     router.push("thank-you");
   };
